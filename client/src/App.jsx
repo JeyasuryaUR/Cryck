9f5d56c8741b8d58146608567ab9d3b3350b5596
jaskrrish <jashehz@gmail.com>
2024-03-29 16:58:22 +0530

    Set Bet Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import MatchBet from "./pages/MatchBet";
 import React from "react";
 
 const App = () => {
   return (
     <div className="">
       <Header />
+      <MatchBet />
     </div>
   );
 };
---
+++ client/src/pages/MatchBet.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MatchBet = () => {
+  return (
+    <div className="flex w-full bg-gray-900">
+      <div className="w-[50%] h-[500px]">
+        <p className="text-center text-white">Your Bet</p>
+      </div>
+      <div className="w-[50%] h-[500px]">
+        <p className="text-center text-white">Next Bet</p>
+      </div>
+    </div>
+  );
+};
+
+export default MatchBet;
