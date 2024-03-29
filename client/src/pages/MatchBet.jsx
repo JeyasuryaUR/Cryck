ec20bd401c8f5d4c56ee47ee9ed5457f064e2244
jaskrrish <jashehz@gmail.com>
2024-03-29 17:14:35 +0530

    Add Dropdown

--- client/src/pages/MatchBet.jsx
+++ client/src/pages/MatchBet.jsx
@@ -13,6 +13,15 @@ const MatchBet = () => {
         </div>
         <div className="w-[50%] h-[500px]">
           <p className="text-center text-white">Next Bet</p>
+          <select
+            id="countries"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option selected>Choose an Option</option>
+            <option value="4">Fours</option>
+            <option value="6">Six</option>
+            <option value="W">Caught Out</option>
+          </select>
         </div>
       </div>
     </div>
