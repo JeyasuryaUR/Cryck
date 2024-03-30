6c4a406600b1f63cf10f00ce2673dd3a4363baf9
jaskrrish <jashehz@gmail.com>
2024-03-30 12:01:29 +0530

    Fix Match Page

--- client/src/pages/MatchBet.jsx
+++ client/src/pages/MatchBet.jsx
@@ -1,23 +1,24 @@
 import React, { useState, useRef } from "react";
+import { background } from "../assets";
 import MatchCard from "../components/MatchCard";
 import MintRedeemInterface from "../components/MintRedeemInterface";
 import { useReadContract, useWriteContract } from "wagmi";
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../config";
-import Web3 from 'web3'
+import Web3 from "web3";
 
 const MatchBet = () => {
   const [selectedTab, setSelectedTab] = useState("Your Bet");
   const [betAmount, setBetAmount] = useState();
-  const {writeContract,error,status} = useWriteContract();
+  const { writeContract, error, status } = useWriteContract();
 
   const totalBets = useReadContract({
-    abi:CONTRACT_ABI,
-    address:CONTRACT_ADDRESS,
-    functionName: 'totalBets',
+    abi: CONTRACT_ABI,
+    address: CONTRACT_ADDRESS,
+    functionName: "totalBets",
   });
 
   const predictions = ["4", "6", "W"];
-  const zones = Array.from({length: 14}, (_, i) => (i + 1).toString());
+  const zones = Array.from({ length: 14 }, (_, i) => (i + 1).toString());
 
   let optId = 1;
   const predictionZoneMap = [];
@@ -27,7 +28,7 @@ const MatchBet = () => {
       predictionZoneMap.push({
         optId: optId++,
         prediction,
-        zone
+        zone,
       });
     }
   }
@@ -35,7 +36,6 @@ const MatchBet = () => {
   const predictionRef = useRef();
   const zoneRef = useRef();
   const ballRef = useRef();
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -45,49 +45,93 @@ const MatchBet = () => {
     const selectedZone = zoneRef.current.value;
     const selectedBall = ballRef.current.value;
 
-    const selectedOption = (predictionZoneMap.find(option => option.prediction === selectedPrediction && option.zone === selectedZone));
+    const selectedOption = predictionZoneMap.find(
+      (option) =>
+        option.prediction === selectedPrediction && option.zone === selectedZone
+    );
 
     if (selectedOption) {
-      const betId = (Number(totalBets.data) - 6 + parseInt(selectedBall)) ; 
-      if(error){
-        alert(error.cause.reason)
+      const betId = Number(totalBets.data) - 6 + parseInt(selectedBall);
+      if (error) {
+        alert(error.cause.reason);
       }
 
-      writeContract({ 
-        abi:CONTRACT_ABI,
+      writeContract({
+        abi: CONTRACT_ABI,
         address: CONTRACT_ADDRESS,
-        functionName: 'bet',
-        args: [betAmountInWei,betId,selectedOption.optId],
-     })
+        functionName: "bet",
+        args: [betAmountInWei, betId, selectedOption.optId],
+      });
     } else {
-      console.log('Yoo')
+      console.log("Yoo");
     }
   };
 
   return (
     <div className="flex bg-black">
+      <img
+        loading="lazy"
+        src={background}
+        className="object-cover absolute inset-0 size-full"
+      />
       <div className="w-[45%]">
         <MatchCard />
       </div>
       <div className="w-[55%] bg-black pr-3">
+        <img
+          src="/zones.png"
+          width={600}
+          className="mx-auto my-1"
+          alt="Field Image for reference"
+        />
         <div className="flex w-full bg-gray-900 mb-5 rounded-md">
-          <div className="w-[33%] rounded-md p-1" onClick={() => setSelectedTab("Next Bet")}>
-            <p className={`text-center text-white ${selectedTab === "Next Bet" ? "bg-gray-700" : ""}`}>Next Bet</p>
+          <div
+            className="w-[33%] rounded-md p-1"
+            onClick={() => setSelectedTab("Next Bet")}
+          >
+            <p
+              className={`text-center text-white ${
+                selectedTab === "Next Bet" ? "bg-gray-700" : ""
+              }`}
+            >
+              Next Bet
+            </p>
           </div>
-          <div className="w-[33%] rounded-md p-1" onClick={() => setSelectedTab("Your Bet")}>
-            <p className={`text-center text-white ${selectedTab === "Your Bet" ? "bg-gray-700" : ""}`}>Your Bet</p>
+          <div
+            className="w-[33%] rounded-md p-1"
+            onClick={() => setSelectedTab("Your Bet")}
+          >
+            <p
+              className={`text-center text-white ${
+                selectedTab === "Your Bet" ? "bg-gray-700" : ""
+              }`}
+            >
+              Your Bet
+            </p>
           </div>
-          <div className="w-[33%] rounded-md p-1" onClick={() => setSelectedTab("Mint CRC")}>
-            <p className={`text-center text-white ${selectedTab === "Mint CRC" ? "bg-gray-700" : ""}`}>Mint Coins</p>
+          <div
+            className="w-[33%] rounded-md p-1"
+            onClick={() => setSelectedTab("Mint CRC")}
+          >
+            <p
+              className={`text-center text-white ${
+                selectedTab === "Mint CRC" ? "bg-gray-700" : ""
+              }`}
+            >
+              Mint Coins
+            </p>
           </div>
         </div>
         {selectedTab === "Next Bet" && (
-          
           <div className="h-full bg-black text-white">
-          <img src="/zones.png" width={600} className="mx-auto my-1" alt="Field Image for reference" />
             <form className="space-y-4" onSubmit={handleSubmit}>
               <div>
-                <label htmlFor="ballNumber" className="block text-sm font-medium text-white">Ball Number</label>
+                <label
+                  htmlFor="ballNumber"
+                  className="block text-sm font-medium text-white"
+                >
+                  Ball Number
+                </label>
                 <select
                   id="ballNumber"
                   required
@@ -95,7 +139,7 @@ const MatchBet = () => {
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-gray-700 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white"
                 >
                   <option value="">Select Ball Number</option>
-                  <option value='0'>Ball 1</option>
+                  <option value="0">Ball 1</option>
                   <option value="1">Ball 2</option>
                   <option value="2">Ball 3</option>
                   <option value="3">Ball 4</option>
@@ -105,7 +149,12 @@ const MatchBet = () => {
               </div>
 
               <div>
-                <label htmlFor="prediction" className="block text-sm font-medium text-white">Prediction</label>
+                <label
+                  htmlFor="prediction"
+                  className="block text-sm font-medium text-white"
+                >
+                  Prediction
+                </label>
                 <select
                   ref={predictionRef}
                   id="prediction"
@@ -120,7 +169,12 @@ const MatchBet = () => {
               </div>
 
               <div>
-                <label htmlFor="zone" className="block text-sm font-medium text-white">Zone</label>
+                <label
+                  htmlFor="zone"
+                  className="block text-sm font-medium text-white"
+                >
+                  Zone
+                </label>
                 <select
                   ref={zoneRef}
                   id="zone"
@@ -144,32 +198,30 @@ const MatchBet = () => {
                   <option value="14">Zone 14</option>
                 </select>
 
-                <input 
-                placeholder="Amount of CRC to bet" 
-                type="number"
-                className='p-2 border rounded my-5 flex-grow bg-gray-700 border-gray-300 text-white outline-none w-full'
-                onChange={(e)=>setBetAmount(e.target.value)}
-               />
+                <input
+                  placeholder="Amount of CRC to bet"
+                  type="number"
+                  className="p-2 border rounded my-5 flex-grow bg-gray-700 border-gray-300 text-white outline-none w-full"
+                  onChange={(e) => setBetAmount(e.target.value)}
+                />
               </div>
 
-              <button type="submit" className="mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <button
+                type="submit"
+                className="mt-4 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black bg-indigo-400 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 Submit
               </button>
             </form>
           </div>
         )}
         {selectedTab === "Your Bet" && (
-          <div className="h-[500px]">
-            {/* Your Bet content goes here */}
-          </div>
+          <div className="h-[500px]">{/* Your Bet content goes here */}</div>
         )}
-        {selectedTab === "Mint CRC" && (
-          <MintRedeemInterface />
-        )}
+        {selectedTab === "Mint CRC" && <MintRedeemInterface />}
       </div>
     </div>
   );
 };
 
 export default MatchBet;
-
