0918bb81e14247419472de40044fbf9056b6415b
JeyasuryaUR <abcdef@gmail.com>
2024-03-30 17:06:31 +0530

    Header & Zones correction

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,13 +4,17 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Header = () => {
   return (
-    <nav className="sticky bg-black w-full top-0 z-50 flex">
-      <div className="m-4 text-white font-bold">
-        <img src={cryckLogo} alt="logo" width={95} />
+    <nav className="sticky bg-black w-full top-0 z-50 flex items-center p-4">
+      <div className="text-white font-bold flex items-center space-x-2">
+        <img src={cryckLogo} alt="logo" className="h-16" />
       </div>
-      <div className="ml-auto m-4 flex items-center">
+      <div className="ml-auto flex items-center space-x-4">
+        <div className="flex items-center space-x-2 text-white">
+          <span>1000</span>
+          <img src={"./crc.png"} alt="CRC" className="h-6" />
+        </div>
         <ConnectButton />
-        <button className="px-4 text-white py-2">Connect Aadhaar</button>
+        <button className="px-4 text-white py-2 rounded bg-indigo-500 hover:bg-indigo-600">Connect Aadhaar</button>
       </div>
     </nav>
   );
